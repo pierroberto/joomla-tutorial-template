f30081876ce62e46b1a9f4dfc3a3936e597953b1
Pier Roberto Lucisano <pierroberto.lucisano@nttdata.com>
2018-07-07 12:42:10 +0200

    fix templateDetails, now it can be installed

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<head>
-<jdoc:include type="head" />
-<link rel="stylesheet" href="<?php echo $this->baseurl ?>templates/mytemplate/css/main.css" type="text/css" />
-</head>
+<?php defined( '_JEXEC' ) or die( 'Restricted access' );?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" 
+   xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
+    <head>
+    <jdoc:include type="head" />
+    <link rel="stylesheet" href="<?php echo $this->baseurl ?>templates/mytemplate/css/main.css" type="text/css" />
+    </head>
+    <body>
+    <jdoc:include type="modules" name="top" /> 
+    <jdoc:include type="component" />
+    <jdoc:include type="modules" name="bottom" />
+    </body>
+</html>
